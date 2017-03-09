9abf50ac83aae8b204333a6d58836241e436d571
moajo <mimirosiasd@gmail.com>
2017-03-10 01:44:29 +0900

    add se.

--- www/scripts/sounds.js
+++ www/scripts/sounds.js
@@ -1,5 +1,11 @@
 const SE = {
   countDown: new Howl({
     src: ['sounds/Countdown06-2.mp3']
-  })
+  }),
+  touch: new Howl({
+    src: ["sounds/select07.mp3"]
+  }),
+  goal: new Howl({
+    src: ["sounds/select01.mp3"]
+  }),
 }
